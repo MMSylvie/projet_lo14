c429f5096b654322bb538268aaefadc18703c136
QuentinLal <36624262+QuentinLal@users.noreply.github.com>
2018-06-14 19:52:45 +0200

    cat, ls, cd
    cat done, ls ../ & ls .. done, cd .. & cd ../ done

--- server.sh
+++ server.sh
@@ -16,7 +16,7 @@ if [ $# -ne 1 ]; then
 fi
 
 PORT="$1"
-
+CURRENT="Exemple/Test/A/A1"
 # The pipe
 FIFO="/tmp/$USER-fifo-$$"
 
@@ -65,7 +65,6 @@ function interaction() {
 	fi
     done
 }
-
 # These functions implements the differents modes available on the server
 function mode-list() {
   echo "[Connection successfull]"
@@ -73,85 +72,175 @@ function mode-list() {
   echo "This is the list of the archives stored on the server..."
   ls archive/
 }
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-pwd()
 {
-  pwd
+  echo "$CURRENT/"
 }
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-cat()
 {
- echo "cat"
+  ARCHIVE=arch.txt
+  grep '^directory' $ARCHIVE | sed 's/directory //g' > dir.txt                         #On recup header
+  if [ $# -eq 0 ]; then
+      echo "We need a file in Argument"
+  elif [ $# -eq 1 ]; then
+      VAR=$1
+  fi
+  touch /tmp/target.txt
+  ligne=$(grep -n '^directory '$CURRENT'' $ARCHIVE | head -1 | cut -d: -f1)            #On recup num de ligne du répertoire actuel
+  search=$(grep -n '^@$' $ARCHIVE | cut -d: -f1)                                       #On recup les numéros de ligne ne contenant que
+  search=$(echo $search | sed 's/ /:/g')                                               #des @ qui délimite les répertoires dans le header
+  pos=$(grep -n '^'$CURRENT'' dir.txt | head -1 | cut -d: -f1)                         #On recup la postition target (Dans la liste complête des répertoires)
+  lastpos=$(echo $search | cut -d: -f$pos)                                             #On recup la dernière ligne prenant le numéro de la ligne du @ correspondant au répertoire à lister
+  nbligne_tot=$(($lastpos-$ligne-1))                                                   #On calcul ensuite le nombre de ligne total
+  cat $ARCHIVE | head -$(($lastpos-1)) | tail -$nbligne_tot >> /tmp/target.txt         #On recup les lignes et on affiche
+  valtot=$(head -1 $ARCHIVE | awk -F: '{print $2}')
+  while read ligne ; do
+      verif=$(echo $ligne | awk '{print $1}')
+      var1=$(echo $ligne | awk '{print $2}' | cut -c 2)
+      var2=$(echo $ligne | awk '{print $2}' | cut -c 1)
+      var3=$(echo $ligne | awk '{print $4}')
+      var4=$(echo $ligne | awk '{print $5}')
+      if [ "$verif" = "$VAR" ]; then
+          if [ "$var2" = "-" ]; then
+              if [ "$var1" = "r" ]; then
+                  cat $ARCHIVE | head -$(($var3-1+$var4-1+valtot)) | tail -$var4
+                  Flagwork="1"
+              else
+                  echo "You need right on file to use cat"
+              fi
+          else
+              echo "The Argument have to be a file"
+          fi
+      fi
+  done < /tmp/target.txt
+  if [ "$Flagwork" != "1" ];then
+      echo "file not found"
+  fi
+  rm /tmp/target.txt
 }
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-cd()
 {
-   echo "cd"
+  ARCHIVE=arch.txt
+  grep '^directory' $ARCHIVE | sed 's/directory //g' > dir.txt                         #On recup header
+  if [ $# -eq 0 ]; then
+      FOLDER="$CURRENT"
+      Flagfinnish="1"
+  elif [ $# -eq 1 ]; then
+      FOLDER=$(echo $1)
+      VAR=$(echo $FOLDER | awk '{print $1}')
+      if [ "$VAR" = "." ]; then
+          FOLDER="$CURRENT"
+      elif [ "$VAR" = ".." ] || [ "$VAR" = "../" ]; then                                   #Si cd .. ou cd ../
+          if [ "$CURRENT" = "Exemple/Test" ]; then
+              FOLDER="$CURRENT"
+              Flagfinnish="1"
+              echo "$FOLDER"
+          else
+              FOLDER=$(echo "$CURRENT" | sed -e 's/^\(.*\/\)[^\/]*/\1/')
+              FOLDER=$(echo "$FOLDER" | sed -e 's/[/]$//g')
+          fi
+      elif [ "$VAR" = "/" ]; then                                                       #Si cd /
+          CURRENT="Exemple/Test/"
+          Flagfinnish="1"
+          echo "$CURRENT"
+      else
+          FOLDER="$CURRENT/$VAR"
+      fi
+  else
+  echo "Need 1 or 0 Argument"
+  fi
+  if [ "$Flagfinnish" != "1" ] ; then
+      if [ "$Flagprevious" != "1" ] ; then
+          ligne=$(grep -n '^directory '$FOLDER'' $ARCHIVE | head -1 | cut -d: -f1)     #On recup num de ligne du répertoire actuel
+          search=$(head -$ligne $ARCHIVE | tail -1 | sed 's/directory //g' )
+          if [ "$search" = "$FOLDER" ] ; then
+              CURRENT=$FOLDER/
+              echo "$CURRENT"
+          elif [ "$search/" = "$FOLDER" ] ; then
+              CURRENT=$FOLDER
+              echo "$CURRENT"
+          else
+              echo "Argument Error"
+          fi
+      fi
+  fi
 }
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-ls()
 {
-
-            grep '^directory' $ARCHIVE | sed 's/directory //g' > dir.txt                  #On recup header
-
-            if [ $# -eq 0 ]; then
-                FOLDER="$CURRENT"
-            elif [ $# -eq 1 ]; then
-                FOLDER=$(echo $1 | sed 's/\/$//g')                                        #On recup l'argument supprime l'éventuelle / en fin de ligne
-                VAR=$(echo $FOLDER | awk -F/ '{print $1}')                                #On récup ./ ou ../ si existe
-                if [ "$VAR" = "." ]; then                                                 #Si ls .
-                    FOLDER="$CURRENT"
-                elif [ "$VAR" = ".." ]; then                                              #Si ls .. ou ls ../...
-                    MASTER=$(echo $CURRENT | sed 's/\(.*\)\/[A-Z a-z 0-9]*$/\1/g')
-                    if [ "$FOLDER" = ".." ]; then
-                        FOLDER="$MASTER"
-                    else
-                        FOCUS=$(echo $FOLDER | sed 's/^\.\.\/\(.*\)$/\1/g')
-                        FOLDER="$MASTER/$FOCUS"
-                    fi
-                else
-                    FOLDER="$CURRENT/$VAR"
-                fi
-            else
-                echo "Need 1 or 0 Argument"
-            fi
-            touch /tmp/target.txt
-
-            ligne=$(grep -n '^directory '$FOLDER'' $ARCHIVE | head -1 | cut -d: -f1)     #On recup num de ligne du répertoire à lister
-            lignedel=$(grep -n '^@$' $ARCHIVE | cut -d: -f1 | tail -$ligne)              #On recup les numéros de ligne ne contenant que
-            lignedel=$(echo $lignedel | sed 's/ /:/g')                                   #des @ qui délimite les répertoires dans le header
-            #pos=$(grep -n '^'$FOLDER'' dir.txt | head -1 | cut -d: -f1)                 #On recup la postition target (Dans la liste complête des répertoires)
-            lastpos=$(echo $lignedel | cut -d: -f$pos)    #$pos                          #On recup la dernière ligne prenant le numéro de la ligne du @ correspondant au répertoire à lister
-            nbligne_tot=$(($lastpos-$ligne-1))                                           #On calcul ensuite le nombre de ligne total
-            cat $ARCHIVE | head -$(($lastpos-1)) | tail -$nbligne_tot >> /tmp/target.txt #On recup les lignes et on affiche
-
-            touch /tmp/targetdir.txt
-            dir=""
-
-            while read ligne ; do
-                var1=$(echo $ligne | awk '{print $2}' | cut -c 4)
-                var2=$(echo $ligne | awk '{print $2}' | cut -c 1)
-                if [ "$var2" = "d" ]; then
-                    dir=$(echo $ligne | awk '{print $1}')
-                    echo "$dir" >> /tmp/targetdir.txt
-                elif [ "$var1" = "x" ]; then
-                    dir=$(echo $ligne | awk '{print $1}')
-                    echo "$dir" >> /tmp/targetdir.txt
-                else
-                    dir=$(echo $ligne | awk '{print $1}')
-                    echo "$dir" >> /tmp/targetdir.txt
-                fi
-            done < /tmp/target.txt
-
-            cat /tmp/targetdir.txt
-            rm  /tmp/target.txt
-            rm  /tmp/targetdir.txt
+  ARCHIVE=arch.txt
+  grep '^directory' arch.txt | sed 's/directory //g' > dir.txt                  #On recup header
+  if [ $# -eq 0 ]; then
+      FOLDER="$CURRENT"
+  elif [ $# -eq 1 ]; then
+      FOLDER=$(echo $1 | sed 's/\/$//g')                                        #On recup l'argument supprime l'éventuelle / en fin de ligne
+      VAR=$(echo $FOLDER | awk -F/ '{print $1}')                                #On récup ./ ou ../ si existe
+      if [ "$VAR" = "." ]; then                                                 #Si ls .
+          FOLDER="$CURRENT"
+      elif [ "$VAR" = ".." ]; then                                              #Si ls .. ou ls ../
+          FOLDER=$(echo "$CURRENT" | sed -e 's/^\(.*\/\)[^\/]*/\1/')
+          FOLDER=$(echo "$FOLDER" | sed -e 's/[/]$//g')
+      elif [ "$VAR" = "-l" ]; then
+          FlagRight="1"
+          FOLDER="$CURRENT"
+      else
+          FOLDER="$CURRENT/$VAR"
+      fi
+  else
+      echo "Need 1 or 0 Argument"
+  fi
+  touch /tmp/target.txt
+  ligne=$(grep -n '^directory '$FOLDER'' $ARCHIVE | head -1 | cut -d: -f1)            #On recup num de ligne du répertoire actuel
+  search=$(grep -n '^@$' $ARCHIVE | cut -d: -f1)                                       #On recup les numéros de ligne ne contenant que
+  search=$(echo $search | sed 's/ /:/g')                                               #des @ qui délimite les répertoires dans le header
+  pos=$(grep -n '^'$FOLDER'' dir.txt | head -1 | cut -d: -f1)                          #On recup la postition target (Dans la liste complête des répertoires)
+  lastpos=$(echo $search | cut -d: -f$pos)                                             #On recup la dernière ligne prenant le numéro de la ligne du @ correspondant au répertoire à lister
+  nbligne_tot=$(($lastpos-$ligne-1))                                                   #On calcul ensuite le nombre de ligne total
+  cat $ARCHIVE | head -$(($lastpos-1)) | tail -$nbligne_tot >> /tmp/target.txt         #On recup les lignes et on affiche
+  touch /tmp/targetdir.txt
+  dir=""
+  while read ligne ; do
+       var1=$(echo $ligne | awk '{print $2}' | cut -c 4)
+       var2=$(echo $ligne | awk '{print $2}' | cut -c 1)
+       if [ "$FlagRight" != "1" ]; then
+           if [ "$var2" = "d" ]; then
+               dir=$(echo $ligne | awk '{print $1}')
+               echo "$dir/" >> /tmp/targetdir.txt
+           elif [ "$var2" = "-" ]; then
+               dir=$(echo $ligne | awk '{print $1}')
+               echo "$dir*" >> /tmp/targetdir.txt
+           else
+               dir=$(echo $ligne | awk '{print $1}')
+               echo "$dir" >> /tmp/targetdir.txt
+           fi
+       else
+           if [ "$var2" = "d" ]; then
+               dir=$(echo $ligne | awk '{print $1,$2}')
+               echo "$dir" >> /tmp/targetdir.txt
+           elif [ "$var2" = "-" ]; then
+               dir=$(echo $ligne | awk '{print $1,$2}')
+               echo "$dir" >> /tmp/targetdir.txt
+           else
+               dir=$(echo $ligne | awk '{print $1,$2}')
+               echo "$dir" >> /tmp/targetdir.txt
+           fi
+       fi
+  done < /tmp/target.txt
+  cat /tmp/targetdir.txt #| awk 1 ORS=' '
+  #echo -e "\n"
+  rm  /tmp/target.txt
+  rm  /tmp/targetdir.txt
+  rm  dir.txt
 }
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-rm()
 {
   echo "rm"
 }
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function mode-extract() {
   echo "[Server] You asked for the extraction of the following archive(s): $args"
 
